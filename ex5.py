10e78094e8b4ddd8d710a2e3387b6a6cbe34a7c6
JordyMC1 <130586651+JordyMC1@users.noreply.github.com>
2023-04-12 11:46:24 -0600

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Jordy Mendoza'
+my_age = 22
+my_height = 1.75
+my_weight = 60
+my_eye = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's ralk about {my_name}.")
+print(f"He's {my_height} meters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eye} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the cofee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.") 
